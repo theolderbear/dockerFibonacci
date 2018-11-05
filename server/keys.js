32e676b96eb108fc13400570ada98442d5e109cb
Paolo Luciani <pinabello@gmail.com>
2018-11-05 17:30:52 +0100

    Initial state

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redishost: process.env.REDIS_HOST,
+    redisport: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgPassword: process.env.PGPASSWORD,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPort: process.env.PGPORT
+};
---
+++ worker/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    redishost: process.env.REDIS_HOST,
+    redisport: process.env.REDIS_PORT
+};
