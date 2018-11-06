7cc845848c48d00970a8766213d3c8c993cb8553
Paolo Luciani <pinabello@gmail.com>
2018-11-06 14:09:36 +0100

    client

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {link} from 'react-router-dome';
+
+export default () => {
+    return (
+        <div>
+            Im some other page!
+        <Link to="/">Go back home</Link>
+        </div>
+
+    );
+};
