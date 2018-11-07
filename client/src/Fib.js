6ddb7adc25b44ce79758d7b4cebbdca153eafdb5
Paolo Luciani <pinabello@gmail.com>
2018-11-07 15:44:33 +0100

    Client finished

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -23,4 +23,52 @@ class Fib extends Component {
         const seeIndexes = await axios.get('/api/values/all');
         this.setState({seeIndexes: seeIndexes.data});
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values', {index: this.state.index});
+
+        this.setState({index: ''});
+    }
+
+    renderSeenIndexes() {
+        return this.state.seeIndexes.map(({number}) => number).join(', ');
+    }
+
+    renderValues() {
+        const entries = [];
+
+        for (let key in this.state.values) {
+            entries.push(
+               <div key={key}>
+                    For index {key} I calculeted {this.state.values[key]}
+               </div>
+            );
+        }
+        return entries;
+    }
+
+    render() {
+        return (
+          <div>
+            <form onSubmit={this.handleSubmit()}>
+                <label>Enter your index</label>
+            <input
+                value = {this.state.index}
+                onChange={event => this.setState({index: event.target.value})}
+        />
+            <button>Submit</button>
+            </form>
+
+            <h3>Indexes I have seen:</h3>
+            {this.renderSeenIndexes()}
+
+            <h3>Calucleted values:</h3>
+            {this.renderValues()}
+            </div>
+        );
+    }
 }
+
+export default Fib;
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {link} from 'react-router-dome';
+import { Link } from 'react-router-dom';
 
 export default () => {
     return (
